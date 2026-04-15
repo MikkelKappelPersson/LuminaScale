0f7f54d9ab1bc956e1929317545d7595067d8ac9
Mikkel Kappel Persson <mikkel@kappelpersson.dk>
2026-04-15 21:44:59 +0200

    gpu

--- scripts/train_dequantization_net.sh
+++ scripts/train_dequantization_net.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=outputs/logs/train_%j.err
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=64G
-#SBATCH --gres=gpu:1,gpu_mem:24G
+#SBATCH --gres=gpu:l40s:1
 #SBATCH --time=48:00:00
 #SBATCH --partition=prioritized
 #SBATCH --account=aau
